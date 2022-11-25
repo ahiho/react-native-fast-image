82efeae76047daa54d420b1f4e18348e45e2f8aa
levanlongktmt <levalongktmt@gmail.com>
2022-11-25 11:27:43 +0700

    fix: build fail

--- android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
@@ -6,6 +6,7 @@ import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
 import com.bumptech.glide.load.engine.cache.LruResourceCache;
 import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.GlideBuilder;
 
 // We need an AppGlideModule to be present for progress events to work.
 @GlideModule
