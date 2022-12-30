3e0b5963926cff48aa7bde3bf44d22d142eb2a3a
levanlongktmt <levalongktmt@gmail.com>
2022-12-30 17:35:58 +0700

    feat: calculate size

--- android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
 import com.bumptech.glide.load.engine.cache.LruResourceCache;
+import com.bumptech.glide.load.engine.cache.MemorySizeCalculator;
 import com.bumptech.glide.module.AppGlideModule;
 import com.bumptech.glide.GlideBuilder;
 
